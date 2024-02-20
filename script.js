5b6a69b95a0503972a68b3d3d9288c2fd1eb3c3f
zeyad220 <159933120+zeyad220@users.noreply.github.com>
2024-02-20 22:16:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showResponseForm() {
+    var responseForm = document.getElementById('responseForm');
+    if (responseForm.style.display === 'none') {
+        responseForm.style.display = 'block';
+    } else {
+        responseForm.style.display = 'none';
+    }
+}
